edac29bd10ff627b5d7986b4ed698cde27d5a990
zzh315 <zhengzh315@gmail.com>
2023-05-10 04:53:37 +1000

    added token expiration

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,33 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+
+  const token = useLoaderData();
+  const submit = useSubmit();
+  const tokenDuration = getTokenDuration();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "POST" });
+      return;
+    }
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "POST" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
