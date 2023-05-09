a1211cb87c1a7406abc06b3e092d1e53ef04d902
zzh315 <zhengzh315@gmail.com>
2023-05-10 04:25:50 +1000

    added route protection for new and edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,8 @@ import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
 import { action as logoutAction } from "./pages/Logout";
-import { tokenLoader } from "./util/auth";
+import { checkAuthToken, tokenLoader } from "./util/auth";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -51,6 +52,7 @@ const router = createBrowserRouter([
                 path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthToken,
               },
             ],
           },
@@ -58,6 +60,7 @@ const router = createBrowserRouter([
             path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthToken,
           },
         ],
       },
